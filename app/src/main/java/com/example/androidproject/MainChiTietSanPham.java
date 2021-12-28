fa94cd4c1c786374cf3a672a709feceb148c9ea3
hieuchuong <hieuchuong321@gmail.com>
2021-12-28 00:37:26 -0800

    layout_chitietsanpham

---
+++ app/src/main/java/com/example/androidproject/MainChiTietSanPham.java
@@ -0,0 +1,20 @@
+package com.example.androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainChiTietSanPham extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_chi_tiet_san_pham);
+        AnhXa();
+    }
+
+    private void AnhXa() {
+
+    }
+}

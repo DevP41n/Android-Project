9ed9d588896d1c7848250d0e58ec1a2f7c213067
sineemxc <76941616+sineemxc@users.noreply.github.com>
2022-01-04 20:38:45 +0700

    Edit User Info

--- app/src/main/java/com/example/androidproject/UserInfo.java
+++ app/src/main/java/com/example/androidproject/UserInfo.java
@@ -2,7 +2,11 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.SharedPreferences.SessionManager;
@@ -13,7 +17,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Optional;
 
 public class UserInfo extends AppCompatActivity {
 
@@ -25,13 +28,36 @@ public class UserInfo extends AppCompatActivity {
     Connection connect;
     String connectionResult = "";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
 
         linkViews();
+        addEvents();
         loadDatas();
+    }
+
+    private void addEvents() {
+        txtHoTen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEditUserDialog(1);
+            }
+        });
+        txtSoDienThoai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEditUserDialog(2);
+            }
+        });
+        txtEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEditUserDialog(3);
+            }
+        });
     }
 
     private void loadDatas() {
@@ -66,4 +92,78 @@ public class UserInfo extends AppCompatActivity {
         txtEmail = findViewById(R.id.txtEmail);
         txtSoDienThoai = findViewById(R.id.txtSoDienThoai);
     }
+
+    private void openEditUserDialog(int id) {
+        TextView txtTitleDialog;
+        EditText edtName;
+        Button btnOK, btnCancel;
+
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_edit_dialog);
+        edtName = dialog.findViewById(R.id.edtName);
+        txtTitleDialog = dialog.findViewById(R.id.txtTitleDialog);
+
+
+
+        SessionManager sessionManager = new SessionManager(UserInfo.this);
+        int userID = sessionManager.getSession();
+        if( id == 1) {
+            txtTitleDialog.setText("Thay đổi tên");
+            edtName.setText(txtHoTen.getText().toString());
+        }
+        if( id == 2) {
+            txtTitleDialog.setText("Thay đổi số điện thoại");
+            edtName.setText(txtSoDienThoai.getText().toString());
+        }
+        if( id == 3) {
+            txtTitleDialog.setText("Thay đổi Email");
+            edtName.setText(txtEmail.getText().toString());
+        }
+        btnOK = dialog.findViewById(R.id.btnOk);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    try{
+                        ConnectionHelper connectionHelper = new ConnectionHelper();
+                        connect = connectionHelper.connectionClass();
+                        if(connect!= null) {
+                            String name = edtName.getText().toString();
+                            if (!name.equals("")) {
+                                if(id == 1) {
+                                    String query = "UPDATE KhachHang SET HoTen = N'" + name + "' WHERE MaKH =" + userID;
+                                    Statement st = connect.createStatement();
+                                    st.executeUpdate(query);
+                                }
+                                if(id == 2) {
+                                    String query = "UPDATE KhachHang SET DienthoaiKH = N'" + name + "' WHERE MaKH =" + userID;
+                                    Statement st = connect.createStatement();
+                                    st.executeUpdate(query);
+                                }
+                                if(id == 3) {
+                                    String query = "UPDATE KhachHang SET Email = N'" + name + "' WHERE MaKH =" + userID;
+                                    Statement st = connect.createStatement();
+                                    st.executeUpdate(query);
+                                }
+                            }
+                            dialog.dismiss();;
+                            loadDatas();
+                        }
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                }
+            }
+        });
+        btnCancel = dialog.findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+
 }
